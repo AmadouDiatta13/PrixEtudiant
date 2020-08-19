ffe5a0bd6173db37ae45ac0a2a897b8e6d91286a
AmadouDiatta13 <69826933+AmadouDiatta13@users.noreply.github.com>
2020-08-19 01:13:37 +0000

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://AmadouDiatta13:Tracymcgrady13@cluster0.v2chu.mongodb.net/Cluster0?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
